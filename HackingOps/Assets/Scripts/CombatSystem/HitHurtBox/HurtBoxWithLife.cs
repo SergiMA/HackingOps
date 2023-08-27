a54fc3d43ff0cb7934649ad3abde642cd2844411
SergiMA <sergimonzoareste@gmail.com>
2023-08-27 14:40:35 +0200

    Health recovery

--- HackingOps/Assets/Scripts/CombatSystem/HitHurtBox/HurtBoxWithLife.cs
+++ HackingOps/Assets/Scripts/CombatSystem/HitHurtBox/HurtBoxWithLife.cs
@@ -8,18 +8,55 @@ namespace HackingOps.CombatSystem.HitHurtBox
         public UnityEvent<float, float> OnNotifyHitWithLife;
 
         [SerializeField] private float _maxLife = 1f;
+        [SerializeField] private float _healthRegeneration = 1f;
+        [SerializeField] private float _healthRegenerationCooldown = 5f;
 
         private float _currentLife;
+        private float _currentHealthRegenerationCooldown;
+
+        private bool _isDead = false;
 
         private void Start()
         {
             _currentLife = _maxLife;
         }
 
+        private void Update()
+        {
+            if (_isDead) return;
+
+            UpdateHealthRegenerationCooldown();
+
+            if (_currentHealthRegenerationCooldown <= 0)
+                RecoverHealthOverTime();
+        }
+
+        private void UpdateHealthRegenerationCooldown()
+        {
+            if (_currentHealthRegenerationCooldown > 0f)
+            {
+                _currentHealthRegenerationCooldown -= Time.deltaTime;
+
+                if (_currentHealthRegenerationCooldown < 0f)
+                    _currentHealthRegenerationCooldown = 0f;
+            }
+        }
+
+        private void RecoverHealthOverTime()
+        {
+            _currentLife += _healthRegeneration * Time.deltaTime;
+            _currentLife = Mathf.Min(_currentLife, _maxLife);
+        }
+
         public override void NotifyHit(float damage = 1f)
         {
+            _currentHealthRegenerationCooldown = _healthRegenerationCooldown;
+
             _currentLife -= damage;
             base.NotifyHit(damage);
+
+            if (_currentLife <= 0)
+                _isDead = true;
 
             OnNotifyHitWithLife?.Invoke(_currentLife, _maxLife);
         }
